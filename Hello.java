93cf53890c6298202ddc004b51b4276fcd630912
MariaMir2 <125823492+MariaMir2@users.noreply.github.com>
2023-03-07 17:53:39 +0100

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,23 @@
+/**
+ * Clase Hello
+ *
+ */
+class Hello
+{
+    /**
+     * Method that does the work 
+     */
+    public void go()
+    {
+        System.out.println("Hello, world");
+    }
+    
+    /**
+     *  main method
+     */
+    public static void main(String[] args)
+    {
+        Hello hi = new Hello();
+        hi.go();
+    }
+}
